c72f274dfc4c4e29a6166d06036a97004dfdedbe
wcollins <william.levi.collins@gmail.com>
2022-11-16 15:18:05 -0500

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,11 +1,11 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.3.0"
 
   required_providers {
 
     alkira = {
       source  = "alkiranet/alkira"
-      version = ">= 0.9.2"
+      version = ">= 0.9.7"
     }
 
   }
